c88bd45dd22add7d3d03fb53207a14f007cc56b7
brucewang <brucewang_xz@163.com>
2025-09-27 13:25:35 +0800

    new dist branch

--- src/solana/connect.ts
+++
@@ -1,9 +0,0 @@
-import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
-
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-const address = new PublicKey("7GgLwWxi6a5XGRdzDwTUatdwWySSmeEwDshTqJ8Jbrh4");
-const balance = await connection.getBalance(address);
-const balanceInSOL = balance / LAMPORTS_PER_SOL;
-
-console.log(`The balance of the account at ${address} is ${balanceInSOL} lamports`);
-console.log(connection, `✅ Connected!`);
